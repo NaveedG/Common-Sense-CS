26c5c988ba2ae0e778b23e55a535bb608703439b
NaveedG <nganjani@protonmail.com>
2020-03-11 17:45:24 -0500

    chapter 8 solution completed

---
+++ chapter_8/reverse_string.js
@@ -0,0 +1,14 @@
+// In your favorite programming language, use a stack to implement an algorithm that reverses a string.
+
+function reverseString(string) {
+  const stringArray = string.split('')
+  const reverseArray = []
+
+  // stack implementation (last in, first out)
+  for (var i = 0; i < string.length; i++) {
+    lastChar = stringArray.pop()
+    reverseArray.push(lastChar)
+  }
+
+  return reverseArray.join('')
+}
