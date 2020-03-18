b83f03d7ba4baa8a0958f1a27ec2c4b6cb7c9f89
NaveedG <nganjani@protonmail.com>
2020-03-18 17:27:20 -0500

    ch. 9 ex. 9 solution completed

---
+++ chapter_9/exercise_9.js
@@ -0,0 +1,13 @@
+// Write a recursive function that accepts two numbers and calculates one by the power of the other. For example, if the numbers were 2 and 5, it would calculate 25. 
+// Do not use any built-in power operations provided by your computer language.
+
+findPower = (x, y, power = y) => {
+  if (x === 1)
+    return power
+  else
+    return findPower(x - 1, y, power * y)
+}
+
+console.log(findPower(1, 5)) // 5
+console.log(findPower(2, 5)) // 25
+console.log(findPower(3, 5)) // 125
