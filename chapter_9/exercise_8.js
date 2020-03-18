90ba0d04d461cac53b0f6a91671e1fbea8fd2461
NaveedG <nganjani@protonmail.com>
2020-03-18 16:26:41 -0500

    ch. ex. 8 tests added

--- chapter_9/exercise_8.js
+++ chapter_9/exercise_8.js
@@ -13,3 +13,10 @@ binarySearch = (arr, value, firstElement = 0, lastElement = arr.length - 1) => {
 
   return null
 }
+
+const arr = [1, 2, 5, 7]
+console.log(binarySearch(arr, 1)) // true
+console.log(binarySearch(arr, 2)) // true
+console.log(binarySearch(arr, 5)) // true
+console.log(binarySearch(arr, 7)) // true
+console.log(binarySearch(arr, 9)) // null
