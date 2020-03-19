440ae19264e7e34bfcc3d449b74d5800a1929dc7
NaveedG <nganjani@protonmail.com>
2020-03-19 16:41:05 -0500

    ch. 9 ex. 10 setup

---
+++ chapter_9/exercise_10.js
@@ -0,0 +1,9 @@
+// Write a recursive function that returns all anagrams of a string (even if the anagrams aren’t words themselves). 
+// For example, the anagrams of cat are cat, cta, act, atc, tac, tca.
+
+findAnagrams = string => {
+  if (string.length <= 1) return [string]
+
+  let anagrams = []
+  return anagrams
+}
