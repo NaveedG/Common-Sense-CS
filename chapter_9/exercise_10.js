3dce39ad504cacd186d47fcceb3411d180d75634
NaveedG <nganjani@protonmail.com>
2020-03-19 16:53:31 -0500

    ch. 9 ex. 10 solution done

--- chapter_9/exercise_10.js
+++ chapter_9/exercise_10.js
@@ -2,8 +2,16 @@
 // For example, the anagrams of cat are cat, cta, act, atc, tac, tca.
 
 findAnagrams = string => {
-  if (string.length <= 1) return [string]
+  if (string.length <= 1) return [string] // base case
 
   let anagrams = []
+  for (let i = 0; i < string.length; i++) {
+    const start = string[i]
+    const nonStart = string.slice(0, i) + string.slice(i + 1)
+    const nonStartAnagrams = findAnagrams(nonStart) // recursion
+    nonStartAnagrams.forEach(anagram => anagrams.push(start + anagram))
+  }
   return anagrams
 }
+
+console.log(findAnagrams('cat')) // [ 'cat', 'cta', 'act', 'atc', 'tca', 'tac' ]
