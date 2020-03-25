b84c5e7659abd02cff1137f0c2ac15f6980ee5fe
NaveedG <nganjani@protonmail.com>
2020-03-25 18:38:09 -0500

    recursive index of solution completed

--- chapter_11/recursive_index_of.rb
+++ chapter_11/recursive_index_of.rb
@@ -27,8 +27,29 @@ class LinkedList
       current_node = current_node.next_node
       current_index += 1
     end while current_node
-    
-      return nil
+
+    return nil
+  end
+
+  # recursive version of #index_of
+  def recursive_index_of(value, current_node = @first_node, current_index = 0) 
+    if current_node.data == value
+      return current_index
+    else
+      recursive_index_of(value, current_node.next_node, current_index + 1) if current_node.next_node
+    end
   end
 
 end
+
+node_1 = Node.new('time')
+node_2 = Node.new('heaven')
+node_3 = Node.new('earth')
+node_1.next_node = node_2
+node_2.next_node = node_3
+
+list = LinkedList.new(node_1)
+p list.recursive_index_of('time') # 0
+p list.recursive_index_of('heaven') # 1
+p list.recursive_index_of('earth') # 2
+p list.recursive_index_of('space') # nil
