af717fd89fef9f5423af0978beb146b29be1fd14
NaveedG <nganjani@protonmail.com>
2020-03-26 16:45:32 -0500

    attach linked lists solution completed

---
+++ chapter_11/attach_linked_lists.rb
@@ -0,0 +1,33 @@
+# Write a method that attaches two linked lists together.
+
+class Node
+
+  attr_accessor :data, :next_node, :previous_node
+
+  def initialize(data)
+    @data = data
+  end
+
+end
+
+class LinkedList
+
+  attr_accessor :first_node, :last_node
+
+  def initialize(first_node = nil, last_node = nil)
+    @first_node = first_node
+    @last_node = last_node
+  end
+
+  def attach_linked_list(linked_list)
+    if !(@first_node)
+      @first_node = linked_list.first_node
+      @last_node = linked_list.last_node
+    else
+      linked_list.first_node.previous_node = @last_node
+      @last_node.next_node = linked_list.first_node
+      @last_node = linked_list.first_node
+    end
+  end
+
+end
