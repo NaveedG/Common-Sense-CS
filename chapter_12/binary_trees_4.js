396dc96407de6cf29691c0d602734a51063793be
NaveedG <nganjani@protonmail.com>
2020-04-02 20:56:39 -0500

    ch. 12 ex. 4 solution completed

---
+++ chapter_12/binary_trees_4.js
@@ -0,0 +1,34 @@
+// The height of a tree is the maximum number of nodes on a path from the root to a leaf node. 
+// Write a function that returns the height of a binary tree.
+
+class Node {
+  constructor(data) {
+    this.data = data
+    this.right = null
+    this.left = null
+  }
+
+  insertNode = data => {
+    if (data > this.data)
+      if (this.right)
+        this.right.insertNode(data)
+      else
+        this.right = new Node(data)
+    else
+      if (this.left)
+        this.left.insertNode(data)
+      else
+        this.left = new Node(data)
+  }
+
+  findHeight = (rightTotal = 0, leftTotal = 0) => {
+    if (this.right) { rightTotal += this.right.findHeight() }
+    if (this.left) { leftTotal += this.left.findHeight() }
+
+    if (rightTotal >= leftTotal)
+      return rightTotal + 1
+    else
+      return leftTotal + 1
+  }
+
+}
